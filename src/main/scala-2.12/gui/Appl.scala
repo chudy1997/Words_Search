4a5fedaef374898a6d91cf387ca609bd073f8c9b
chudy1997 <karolbartyzel@interia.pl>
2017-06-25 22:27:11 +0200

    Added guis

--- src/main/scala-2.12/gui/Appl.scala
+++ src/main/scala-2.12/gui/Appl.scala
@@ -1,18 +1,14 @@
 package gui
 
 import app.Categories.ANIMALS
+import app.{Utils, WordsGenerator}
 
 import scalafx.application.JFXApp
-import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.effect.{DropShadow, ImageInput}
-import scalafx.scene.layout.HBox
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.paint.{Color, LinearGradient, Stops}
 import scalafx.scene.text.Text
-import scalafx.scene.image.{Image, ImageView}
-import app.WordsGenerator
-
-import scalafx.scene.control.Label
 
 /**
   * Created by Karol on 2017-06-24.
@@ -20,58 +16,53 @@ import scalafx.scene.control.Label
 
 object Appl extends JFXApp{
   stage = new JFXApp.PrimaryStage {
-    title = "CrossWords"
+    title = "Words Search"
     scene = new Scene(800,600) {
-     fill = Color.rgb(38, 38, 38)
-      content = List(new HBox {
-        padding = Insets(18,700,18,100)
-        children = Seq(
-          new Text {
-            text = "CROSS"
-            style = "-fx-font: normal bold 60pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Color.Yellow, Color.DarkGoldenrod))
-            effect = new DropShadow {
-              color = Color.Green
-              radius = 40
-              spread = 0.25
-            }
-          },
-          new Text {
-            text = "WORDS"
-            style = "-fx-font: italic bold 60pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Color.White, Color.DarkCyan)
-            )
-            effect = new DropShadow {
-              color = Color.Green
-              radius = 40
-              spread = 0.25
-            }
-          }
-        )
+      fill = Color.rgb(38, 38, 38)
+      val titleText1=new Text(70,82,"WORDS"){
+        style = "-fx-font: normal bold 60pt sans-serif"
+        fill = new LinearGradient(
+          endX = 0,
+          stops = Stops(Color.Yellow, Color.DarkGoldenrod))
+        effect = new DropShadow {
+          color = Color.Green
+          radius = 40
+          spread = 0.25
         }
-      )
-      //...............
+      }
+      val titleText2=new Text(370,82,"SEARCH"){
+        style = "-fx-font: italic bold 60pt sans-serif"
+        fill = new LinearGradient(
+          endX = 0,
+          stops = Stops(Color.White, Color.DarkCyan)
+        )
+        effect = new DropShadow {
+          color = Color.Green
+          radius = 40
+          spread = 0.25
+        }
+      }
+
+      content=List(titleText1,titleText2)
+
       var boardCells = List[ImageView]()
       val generator=new WordsGenerator
-      val arr = generator.buildBoard(ANIMALS,2, 10)
+      val arr = generator.buildBoard(1,ANIMALS,2, 10)
+      val utils = new Utils
+      utils.printMapConsole(arr, 10)
       for(i <- 0 until 10) {
         for (j <- 0 until 10) {
-         // val image = if (i==j) new Image("file:Images/a2.png") else new Image("file:Images/b.png")
-          val image = new Image("file:Images/"+arr(i)(j)+".png")
-          val cell = new ImageView(image){
-            layoutX = 400 - 5* 36 - 1 + i * 36
-            layoutY = 120 + j * 36
+          val image =  new Image("file:Images/"+arr(i)(j)+".png")
+          val cell = new ImageView(image) {
+            layoutX = 400 - 5 * 36 - 1 + j * 36
+            layoutY = 120 + i * 36
+            onMouseClicked = {_ => image = new Image("file:Images/"+arr(i)(j)+"2.png")}
           }
 
           boardCells ::= cell
           content.add(cell)
         }
       }
-      //...........
     }
   }
 }
