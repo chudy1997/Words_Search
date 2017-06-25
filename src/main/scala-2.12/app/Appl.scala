29913eed6ea1c4ee4b8b3253d5e0d4917044f5b5
Regulareveryday <julia.sypien@gmail.com>
2017-06-25 13:00:02 +0200

    boardDone

--- src/main/scala-2.12/app/Appl.scala
+++ src/main/scala-2.12/app/Appl.scala
@@ -7,7 +7,7 @@ import app.Categories._
 
 object Appl extends App{
   val n=2
-  val size=6
+  val size=10
   val utils=new Utils
   val generator=new WordsGenerator
   utils.printMapConsole(generator.buildBoard(ANIMALS,n,size),size)
--- src/main/scala-2.12/gui/Appl.scala
+++ src/main/scala-2.12/gui/Appl.scala
@@ -1,12 +1,18 @@
 package gui
 
+import app.Categories.ANIMALS
+
 import scalafx.application.JFXApp
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.effect.DropShadow
+import scalafx.scene.effect.{DropShadow, ImageInput}
 import scalafx.scene.layout.HBox
 import scalafx.scene.paint.{Color, LinearGradient, Stops}
 import scalafx.scene.text.Text
+import scalafx.scene.image.{Image, ImageView}
+import app.WordsGenerator
+
+import scalafx.scene.control.Label
 
 /**
   * Created by Karol on 2017-06-24.
@@ -16,7 +22,7 @@ object Appl extends JFXApp{
   stage = new JFXApp.PrimaryStage {
     title = "CrossWords"
     scene = new Scene(800,600) {
-      fill = Color.rgb(38, 38, 38)
+     fill = Color.rgb(38, 38, 38)
       content = List(new HBox {
         padding = Insets(18,700,18,100)
         children = Seq(
@@ -48,6 +54,24 @@ object Appl extends JFXApp{
         )
         }
       )
+      //...............
+      var boardCells = List[ImageView]()
+      val generator=new WordsGenerator
+      val arr = generator.buildBoard(ANIMALS,2, 10)
+      for(i <- 0 until 10) {
+        for (j <- 0 until 10) {
+         // val image = if (i==j) new Image("file:Images/a2.png") else new Image("file:Images/b.png")
+          val image = new Image("file:Images/"+arr(i)(j)+".png")
+          val cell = new ImageView(image){
+            layoutX = 400 - 5* 36 - 1 + i * 36
+            layoutY = 120 + j * 36
+          }
+
+          boardCells ::= cell
+          content.add(cell)
+        }
+      }
+      //...........
     }
   }
 }
